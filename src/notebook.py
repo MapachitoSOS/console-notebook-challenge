1c26d471be574f268bda49f47e549f84653b3816
Emmanuel Velez Valencia <emmanuelvelezvalencia@gmail.com>
2026-02-16 18:54:35 -0500

    primer commit

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,23 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+class Note:
+    HIGH = "HIGH"
+    MEDIUM = "MEDIUM"
+    LOW = "LOW"
+
+    def __init__(self, code: str, title: str, text: str, importance: str):
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+        self.creation_date: datetime = datetime.now()
+        self.tags: list[str] = []
+
+    def add_tag(self, tag:str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return f"-- {self.code} -- {self.title} -- {self.text} -- {self.importance}"
+
