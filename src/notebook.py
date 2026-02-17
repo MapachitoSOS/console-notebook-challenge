b371257cfd2cb9b4ab7ed4e87dfa4f34d265f425
Emmanuel Velez Valencia <emmanuelvelezvalencia@gmail.com>
2026-02-16 19:36:34 -0500

    segundo commit

--- src/notebook.py
+++ src/notebook.py
@@ -6,7 +6,7 @@ class Note:
     MEDIUM = "MEDIUM"
     LOW = "LOW"
 
-    def __init__(self, code: str, title: str, text: str, importance: str):
+    def __init__(self, code: int, title: str, text: str, importance: str):
         self.code = code
         self.title = title
         self.text = text
@@ -20,4 +20,27 @@ class Note:
 
     def __str__(self):
         return f"-- {self.code} -- {self.title} -- {self.text} -- {self.importance}"
+
+class Notebook:
+    def __init__(self):
+        self.notes: list[Note] = []
+
+    def add_note(self, title: str, text: str, importance: str) -> int:
+        code = len(self.notes) + 1
+        while any(note.code == code for note in self.notes): #uso any para el asegurarme de no haber un codigo repetido con false y true
+            code += 1
+        note = Note(code, title, text, importance)
+        self.notes.append(note)
+        return code
+
+    def delete_note(self, code: int):
+        self.notes.pop(code)
+
+    def important_notes(self) -> list[Note]:
+        return [note for note in self.notes if note.importance in (note.HIGH, note.LOW)]
+
+    def notes_by_tags(self, tag: str) -> list[Note]:
+        return [note for note in self.notes if tag in note.tags]
+
+    def tag_with_most_notes(self) -> str:
 
